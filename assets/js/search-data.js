9cd99f059458a672960352f6effbf6d83360d40e
HJY-CD <54421842+HJY-CD@users.noreply.github.com>
2026-04-15 09:22:46 +0000

    Deploying to gh-pages from @ HJY-CD/HJY-CD@ce8b51a63b5d02880265dcb6409150d0f4d1936e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,26 +16,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/HJY-CD/publications/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/HJY-CD/repositories/";
-          },
         },{id: "nav-cv",
           title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/HJY-CD/cv/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/HJY-CD/people/";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
